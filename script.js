b0d282d7099b66b0822d2032ee263e5b71b5e002
JacksonRMS <jacktjrubem@gmail.com>
2024-06-28 17:44:21 -0300

    Inicio do projeto 'DevSorteio'

---
+++ script.js
@@ -0,0 +1,13 @@
+const betz = document.querySelector(".betz")
+betz.addEventListener("click", sort)
+
+
+
+function sort() {
+
+    const min = document.querySelector('.number-min').value
+    const max = document.querySelector('.number-max').value
+
+
+    console.log(min, max)
+}
