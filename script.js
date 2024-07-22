c294efbbca809c25b8a179623309536ddd31f504
JacksonRMS <jacktjrubem@gmail.com>
2024-07-22 11:20:28 -0300

    Adição do input de resultado

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-const betz = document.querySelector(".betz")
-betz.addEventListener("click", sort)
+// const betz = document.querySelector(".betz")
+// betz.addEventListener("click", sort)
+
+
+// function sort() {
+//     const resultS = document.querySelector('#result').value
+//     const min = Math.ceil(document.querySelector('.number-min').value)
+//     const max = Math.floor(document.querySelector('.number-max').value)
+
+//     const result = Math.floor(Math.random() * (max - min + 1) + min)
+
+//  resultS.innerHTML = (result + "seu")
+// }
+
+
+
+
+
+const betz = document.querySelector(".betz");
+betz.addEventListener("click", sort);
 
 function sort() {
-
-    const min = Math.ceil(document.querySelector('.number-min').value)
-    const max = Math.floor(document.querySelector('.number-max').value)
+    const resultS = document.querySelector('#result');
+    const min = Math.ceil(document.querySelector('.number-min').value);
+    const max = Math.floor(document.querySelector('.number-max').value);
 
     const result = Math.floor(Math.random() * (max - min + 1) + min);
 
-    alert(result)
+    // Exibindo o resultado no campo de input
+    resultS.value = result;
 }
